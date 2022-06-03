9ae691209f957d0ec09e40966f7f0a8d8536dfb9
AndrewFortune <106268242+AndrewFortune@users.noreply.github.com>
2022-06-03 14:08:13 -0400

    Update Drew

---
+++ Unity/The Legend of Gordon/Assets/Scripts/VibeChecker.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibeChecker : MonoBehaviour
+{
+    public GameObject[] Characters;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
--- Unity/The Legend of Gordon/Assets/Scripts/gordonWalk.cs
+++ Unity/The Legend of Gordon/Assets/Scripts/gordonWalk.cs
@@ -7,6 +7,8 @@ public class gordonWalk : MonoBehaviour
     public float speed = 5;
     float horizontalMove = 0;
     public Animator myAnim;
+    public int Attack = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,9 +26,42 @@ public class gordonWalk : MonoBehaviour
         {
             GetComponent<Rigidbody2D>().velocity = new Vector3(0, -5, 0);
         }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 5, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(-5, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(5, 0, 0);
+        }
         else
         {
             GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
         }
+        if(Input.GetKey(KeyCode.Q))
+        {
+            StartCoroutine(AttackActionQ());
+
+        }
+        if(Input.GetKey(KeyCode.Z))
+        {
+            StartCoroutine(AttackActionZ());
+        }
+    }
+    IEnumerator AttackActionQ()
+    {
+        Attack = 1;
+        yield return new WaitForSeconds(0.8F);
+        Attack = 0;
+    }
+    IEnumerator AttackActionZ()
+    {
+        Attack = 2;
+        yield return new WaitForSeconds(0.8F);
+        Attack = 0;
     }
 }
